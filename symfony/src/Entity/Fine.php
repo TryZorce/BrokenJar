59e1f497176011d95e8623c448fe59eae56bf403
TryZorce <christopher.moron@gmail.com>
2024-05-27 15:47:21 +0200

    first init

---
+++ symfony/src/Entity/Fine.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\FineRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FineRepository::class)]
+#[ApiResource]
+class Fine
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $code = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?float $value = null;
+
+    #[ORM\Column]
+    private ?bool $pay = null;
+
+    #[ORM\ManyToOne(inversedBy: 'fines')]
+    private ?User $email = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    public function setCode(string $code): static
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getValue(): ?float
+    {
+        return $this->value;
+    }
+
+    public function setValue(float $value): static
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    public function isPay(): ?bool
+    {
+        return $this->pay;
+    }
+
+    public function setPay(bool $pay): static
+    {
+        $this->pay = $pay;
+
+        return $this;
+    }
+
+    public function getEmail(): ?User
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?User $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+}
---
+++ symfony/src/Entity/User.php
@@ -0,0 +1,259 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use ApiPlatform\Metadata\ApiResource;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: '`user`')]
+#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+#[ApiResource]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 180)]
+    private ?string $email = null;
+
+    /**
+     * @var list<string> The user roles
+     */
+    #[ORM\Column]
+    private array $roles = [];
+
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
+    private ?string $password = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $firstname = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $address = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $phone = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $card = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $crypto = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $expiry = null;
+
+    /**
+     * @var Collection<int, Fine>
+     */
+    #[ORM\OneToMany(targetEntity: Fine::class, mappedBy: 'email')]
+    private Collection $fines;
+
+    public function __construct()
+    {
+        $this->fines = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     *
+     * @return list<string>
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    /**
+     * @param list<string> $roles
+     */
+    public function setRoles(array $roles): static
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials(): void
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): static
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): static
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(string $phone): static
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    public function getCard(): ?string
+    {
+        return $this->card;
+    }
+
+    public function setCard(string $card): static
+    {
+        $this->card = $card;
+
+        return $this;
+    }
+
+    public function getCrypto(): ?string
+    {
+        return $this->crypto;
+    }
+
+    public function setCrypto(string $crypto): static
+    {
+        $this->crypto = $crypto;
+
+        return $this;
+    }
+
+    public function getExpiry(): ?string
+    {
+        return $this->expiry;
+    }
+
+    public function setExpiry(string $expiry): static
+    {
+        $this->expiry = $expiry;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Fine>
+     */
+    public function getFines(): Collection
+    {
+        return $this->fines;
+    }
+
+    public function addFine(Fine $fine): static
+    {
+        if (!$this->fines->contains($fine)) {
+            $this->fines->add($fine);
+            $fine->setEmail($this);
+        }
+
+        return $this;
+    }
+
+    public function removeFine(Fine $fine): static
+    {
+        if ($this->fines->removeElement($fine)) {
+            // set the owning side to null (unless already changed)
+            if ($fine->getEmail() === $this) {
+                $fine->setEmail(null);
+            }
+        }
+
+        return $this;
+    }
+}
